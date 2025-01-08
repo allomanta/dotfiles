16cb6f388372eab1d40caba670f0de48a33dd94d
Steven Lageveen <stevenmax888@gmail.com>
2025-01-08 22:49:08 +0100

    Updated rerun script

--- .scripts/rerun.sh
+++ .scripts/rerun.sh
@@ -7,9 +7,17 @@ function execute() {
     eval "$@"
 }
 
-execute "$@"
-
-inotifywait --quiet --recursive --monitor --event modify --format "%w%f" . \
-| while read change; do
+if [[ $1 == 1 ]]; then
+    execute "${@:2}"
+    inotifywait --quiet --recursive --monitor --event modify --format "%w%f" . \
+    | while read change; do
+        execute "${@:2}"
+    done
+else
     execute "$@"
-done
+    inotifywait --quiet --monitor --event modify --format "%w%f" . \
+    | while read change; do
+        execute "$@"
+    done
+fi
+
