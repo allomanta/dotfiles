b5cb45a41bc04a5bc4fa41ebc7d5897e4b147dfe
Steven Lageveen <stevenmax888@gmail.com>
2025-01-09 14:37:51 +0100

    Made output of fd for fzf relative

--- .scripts/fd-for-fzf.sh
+++ .scripts/fd-for-fzf.sh
@@ -1,2 +1,2 @@
 root="$(git rev-parse --show-toplevel 2>/dev/null)"
-fd . "${root:-./}" -x printf "{}: \033[3m\033[38;5;103m{//}/\033[38;5;177m{/}\033[0m %s\n"
+fd . --strip-cwd-prefix --base-directory "${root:-./}" -x printf "{}: \033[3m\033[38;5;103m{//}/\033[38;5;177m{/}\033[0m %s\n"
