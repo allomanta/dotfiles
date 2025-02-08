8287f6dbbfeb8970e2b7827b21586b47a7370dae
Steven Lageveen <stevenmax888@gmail.com>
2025-02-08 17:23:57 +0100

    Added window rules to open floating for PIP and save dialogues

--- .config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+++ .config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
@@ -29,7 +29,7 @@ path="$4"
 out="$5"
 cmd="yazi"
 # "wezterm start --always-new-process" if you use wezterm
-termcmd="wezterm start --always-new-process"
+termcmd="wezterm start --class float --always-new-process"
 # change this to "/tmp/xxxxxxx/.last_selected" if you only want to save last selected location
 # in session (flushed after reset device)
 last_selected_path_cfg="$HOME/.config/xdg-desktop-portal-termfilechooser/.last_selected"
