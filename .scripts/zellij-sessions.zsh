f80324f4da734c06b3e5e9000067f57c9a4f2f57
Steven Lageveen <stevenmax888@gmail.com>
2025-03-13 17:26:44 +0100

    Better ergonomics for opening new zellij session

--- .scripts/zellij-sessions.zsh
+++ .scripts/zellij-sessions.zsh
@@ -1,12 +1,12 @@
 ZJ_SESSIONS=$(zellij list-sessions -s | grep -v "Tmp" )
 
 if [ "{$ZELLIJ}" ] && [ -z "${ZELLIJ_SESSION_NAME}" ]; then
-  echo ${ZJ_SESSIONS}| fzf --border --border-label "Choose/create Zellij session (blank for tmp)" --layout reverse --height 50% --print-query | { read INPUT; read SELECTION; }
+  echo ${ZJ_SESSIONS}| fzf --border --border-label "Choose/create Zellij session (esc for tmp, alt-enter for new)" --layout reverse --height 50% --bind 'alt-enter:print-query' | read INPUT
 
-  if [ -z "${INPUT}"]; then
+  if [ -z "${INPUT}" ]; then
     SESSION="Tmp-$(date +%s)"
   else
-    SESSION=$SELECTION
+    SESSION=$INPUT
   fi 
 
   zellij attach -c "${SESSION}"
