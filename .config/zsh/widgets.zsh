7de0fa4b7a50a6af06d4292d4b8a50be20f448da
Steven Lageveen <stevenmax888@gmail.com>
2026-03-24 20:57:49 +0100

    zsh helix mode update + test script

--- .config/zsh/widgets.zsh
+++ .config/zsh/widgets.zsh
@@ -1,1 +1,15 @@
+test() {
+  # region_highlight="0 6 fg=green memo=zsh-syntax-highlighting"
+  # region_highlight=( "${region_highlight:#*memo=hm}" )
+  region_highlight=(
+    "${(@)region_highlight:#*memo=hm}"
+    "2 10 bg=red memo=hm"
+  )
+  zle -R
+
+  # echo "testing: $region_highlight"
+  # echo "testing: $zle_highlight"
+  # echo "testing: ${zle_highlight[(r)${entry}:*]-}"
+}
+zle -N test
 
