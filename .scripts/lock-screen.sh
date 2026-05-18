faa273929069fa00b70e4a2fcac6c0f3c1e3f9a5
Steven Lageveen <stevenmax888@gmail.com>
2026-05-18 18:58:55 +0200

    Fix idle timeout

--- .scripts/lock-screen.sh
+++ .scripts/lock-screen.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-swayidle timeout 600 'swaylock -C $HOME/.config/swaylock/config.idle' timeout 1000 'systemctl suspend' before-sleep 'swaylock -C $HOME/.config/swaylock/config.lock' lock 'swaylock -C $HOME/.config/swaylock/config.lock'
+swayidle -w \
+  timeout 600 '$HOME/.scripts/lock.sh' \
+  timeout 1000 'systemctl suspend-then-hibernate' \
+  before-sleep '$HOME/.scripts/lock.sh'
+
+  
+# swayidle timeout 600 'swaylock -C $HOME/.config/swaylock/config.idle' timeout 1000 'systemctl suspend' before-sleep 'swaylock -C $HOME/.config/swaylock/config.lock' lock 'swaylock -C $HOME/.config/swaylock/config.lock'
